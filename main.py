03d53dc9ebbe83730d3b8b403b27597a012293c3
minkang1223 <minkang1223@gmail.com>
2026-05-13 13:50:35 +0900

    Fix string concatenation in greeting message

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ st.title('나의 첫 웹 서비스 만들기!')
 st.text_input('이름을 입력해주세요') 
 st.selectbox('좋아하는 음식을 선택하세요!',['튀김','햄버거','과자'])
 if st.button('인사말 생성'):
-  st.write(a+님! 안녕하세요!') 
+  st.write(a+'님! 안녕하세요!') 
   st.info('반갑습니다') 
   st.warning(b+'음식을 좋아하시는군요') 
   st.error('잘 부탁해요!') 
